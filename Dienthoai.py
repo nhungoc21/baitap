8e79b60c25691a0d9fbd3116bdc7536de3d5f8d7
nhungoc21 <ngocutn21406@st.uel.edu.vn>
2021-12-14 08:56:05 +0700

    Update Dienthoai.py

--- Dienthoai.py
+++ Dienthoai.py
@@ -3,25 +3,16 @@ dsdt = ['ip13', 'ip13pro', 'ip13promax', 'ip13mini']
 dsgia = [2000, 3000, 5000, 500]
 dssl = []
 dsdoanhthu = []
-vitri = []
 listgia = []
 while True: 
-    loai = input('Nhap loai dien thoai: ')
+    loai = int(input('Nhap loai dien thoai:\n1. IPhone 13\n2. IPhone 13 Pro\n3. IPhone 13 Pro Max\n4. IPhone 13 Mini\n'))
+    listgia.append(dsgia[loai - 1])
     sl = int(input('Nhap so luong: '))
     dssl.append(sl)
-    #lay vi tri
-    for dt in dsdt:
-        if dt == loai:
-            vt=dsdt.index(dt)
-            vitri.append(vt)
-    print(vitri)
-    
     t = input('Nhap tiep?(co/khong) ')
     if t == 'khong':
         break  
 #lay gia
-for i in range(len(vitri)):
-    listgia.append(dsgia[vitri[i]])
 print(listgia)
 print(dssl)
 for i in range(len(dssl)):
